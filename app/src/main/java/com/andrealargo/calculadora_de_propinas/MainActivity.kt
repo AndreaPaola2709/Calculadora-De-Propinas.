edd64179b279e7699f2e6dc1e541fdbdeb49de5b
Andrea <andrealargo2709@gmail.com>
2026-05-21 16:15:14 -0500

    implemente el calculo de propina

--- app/src/main/java/com/andrealargo/calculadora_de_propinas/MainActivity.kt
+++ app/src/main/java/com/andrealargo/calculadora_de_propinas/MainActivity.kt
@@ -5,10 +5,15 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -16,8 +21,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.andrealargo.calculadora_de_propinas.ui.theme.CalculadoradepropinasTheme
 
 class MainActivity : ComponentActivity() {
@@ -62,5 +69,58 @@ fun CalculadoraDePropinas(){
         horizontalAlignment = Alignment.CenterHorizontally
 
     ){
-        
+
+        Text(
+            text = "Calculadora de Propinas",
+            fontSize = 24.sp //sp scale independent pixels, unidad de medida adaptable
+        )
+
+        //Espacio entre elementos
+        Spacer(modifier = Modifier.height(20.dp))
+
+        TextField(
+            value = cantidadCuenta,
+            onValueChange = {
+                cantidadCuenta = it
+
+            },
+
+            label = {
+                Text("Escriba el valor de su cuenta")
+            },
+
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number
+            )
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        TextField(
+            value = porcentDePropina,
+            onValueChange = {
+                porcentDePropina = it
+            },
+
+            label = {
+                Text("Escriba el % de la propina")
+            },
+
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number
+            )
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Button(
+            onClick = {
+                val valor = cantidadCuenta.toDoubleOrNull() ?: 0.0
+                val porcentaje = porcentDePropina.toDoubleOrNull() ?: 0.0
+
+                resp = valor * porcentaje / 100
+            }
+        ) {
+ 
+
 
